6bc122cc967d841259ee8b08d80a71d0c64ffb76
Harshak Saini <harshaksaini@gmail.com>
2024-06-06 13:54:55 +0530

    initial commit

--- backend/connect/connect.js
+++ backend/connect/connect.js
@@ -1,5 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/Employee").then(() => {
-  console.log(`Connection Succefully`);
-});
+mongoose
+	.connect(
+		'mongodb+srv://harshaksaini:rs9MZfv4n5Qngrbp@cluster0.7innx4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+	)
+	.then(() => {
+		console.log(`Connection Succefully`);
+	});
